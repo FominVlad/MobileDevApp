1dbb2cb0f6deadc0bd3668bc60a1301167690fb8
Влад Фомин <mr.vladyslavfomin@gmail.com>
2020-04-10 15:16:03 +0300

    .

---
+++ MobileDevApp/MobileDevApp/MobileDevApp.Android/obj/Debug/90/android/src/crc6417df8a0022f517b4/MainActivity.java
@@ -0,0 +1,94 @@
+package crc6417df8a0022f517b4;
+
+
+public class MainActivity
+	extends crc643f46942d9dd1fff9.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.tasks.OnSuccessListener,
+		com.google.android.gms.tasks.OnFailureListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onResume:()V:GetOnResumeHandler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_onActivityResult:(IILandroid/content/Intent;)V:GetOnActivityResult_IILandroid_content_Intent_Handler\n" +
+			"n_onSuccess:(Ljava/lang/Object;)V:GetOnSuccess_Ljava_lang_Object_Handler:Android.Gms.Tasks.IOnSuccessListenerInvoker, Xamarin.GooglePlayServices.Tasks\n" +
+			"n_onFailure:(Ljava/lang/Exception;)V:GetOnFailure_Ljava_lang_Exception_Handler:Android.Gms.Tasks.IOnFailureListenerInvoker, Xamarin.GooglePlayServices.Tasks\n" +
+			"";
+		mono.android.Runtime.register ("MobileDevApp.Droid.MainActivity, MobileDevApp.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("MobileDevApp.Droid.MainActivity, MobileDevApp.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onResume ()
+	{
+		n_onResume ();
+	}
+
+	private native void n_onResume ();
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void onActivityResult (int p0, int p1, android.content.Intent p2)
+	{
+		n_onActivityResult (p0, p1, p2);
+	}
+
+	private native void n_onActivityResult (int p0, int p1, android.content.Intent p2);
+
+
+	public void onSuccess (java.lang.Object p0)
+	{
+		n_onSuccess (p0);
+	}
+
+	private native void n_onSuccess (java.lang.Object p0);
+
+
+	public void onFailure (java.lang.Exception p0)
+	{
+		n_onFailure (p0);
+	}
+
+	private native void n_onFailure (java.lang.Exception p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
