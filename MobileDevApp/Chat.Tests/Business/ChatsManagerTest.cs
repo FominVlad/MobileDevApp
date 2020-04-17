f0b1e9185ae8cd7f43e3ee7649f95372ff387577
Настя Каспрук <anastasiyakaspruk17@gmail.com>
2020-04-17 15:45:14 +0300

    added tests

--- MobileDevApp/Chat.Tests/Business/ChatsManagerTest.cs
+++ MobileDevApp/Chat.Tests/Business/ChatsManagerTest.cs
@@ -1,4 +1,6 @@
-﻿using Chat.DAL.Interfaces;
+﻿using Chat.Business.Implementations;
+using Chat.Business.Models;
+using Chat.DAL.Interfaces;
 using Chat.DAL.Models;
 using Moq;
 using NUnit.Framework;
@@ -12,17 +14,8 @@ namespace Chat.Tests.Business
     [TestFixture]
     public class ChatsManagerTest
     {
-        private readonly Mock<IChatUnitOfWork> _mockChatUnitOfWork;
-
-        public ChatsManagerTest()
-        {
-            _mockChatUnitOfWork = new Mock<IChatUnitOfWork>();
-        }
-
-        [Test]
-        public void GetAllChatsTest()
-        {
-            List<ChatEntity> testChatEntities = new List<ChatEntity>
+        private static readonly List<ChatEntity> _testChatEntities = 
+            new List<ChatEntity>
             {
                 new ChatEntity
                 {
@@ -44,28 +37,98 @@ namespace Chat.Tests.Business
                 },
                 new ChatEntity
                 {
-                    ChatID = 1,
-                    SenderID = 1,
-                    ReceiverID = 2,
+                    ChatID = 2,
+                    SenderID = 2,
+                    ReceiverID = 3,
                     Users = new List<ChatUser>
                     {
-                        new ChatUser { ChatID = 1, UserID = 1, User = new User { UserID = 1 }},
-                        new ChatUser { ChatID = 1, UserID = 2, User = new User { UserID = 2 }}
+                        new ChatUser { ChatID = 2, UserID = 2, User = new User { UserID = 2 }},
+                        new ChatUser { ChatID = 2, UserID = 3, User = new User { UserID = 3 }}
                     },
                     Messages = new List<Message>
                     {
-                        new Message { ChatID = 1, MessageID = 1, Text = "Hi!",
-                            ReceivedDate = DateTime.Now.AddMinutes(-30), SenderID = 1, IsRead = true },
-                        new Message { ChatID = 1, MessageID = 2, Text = "Hello!",
-                            ReceivedDate = DateTime.Now.AddMinutes(-25), SenderID = 2, IsRead = true },
+                        new Message { ChatID = 2, MessageID = 1, Text = "Hi!",
+                            ReceivedDate = DateTime.Now.AddMinutes(-36), SenderID = 3, IsRead = true },
+                        new Message { ChatID = 2, MessageID = 2, Text = "How are you?",
+                            ReceivedDate = DateTime.Now.AddMinutes(-35), SenderID = 3, IsRead = true },
+                        new Message { ChatID = 2, MessageID = 3, Text = "Fine)",
+                            ReceivedDate = DateTime.Now.AddMinutes(-33), SenderID = 2, IsRead = false },
                     }
                 }
             };
 
+        private readonly Mock<IChatUnitOfWork> _mockChatUnitOfWork;
+
+        public ChatsManagerTest()
+        {
+            _mockChatUnitOfWork = new Mock<IChatUnitOfWork>();
+        }
+
+        [Test]
+        public void GetAllChatsTest()
+        {
+            // Arrange
             _mockChatUnitOfWork.Setup(uow => uow.ChatsRepository.FindAll(
-                It.IsAny<ExpressionSpecification<ChatEntity>>(), 
+                It.IsAny<ExpressionSpecification<ChatEntity>>(),
                 It.IsAny<Func<IQueryable<ChatEntity>, IQueryable<ChatEntity>>>()))
-                .Returns()
+                .Returns(_testChatEntities);
+
+            var chatsManager = new ChatsManager(_mockChatUnitOfWork.Object);
+
+            // Act
+            int searchUserId = 2;
+            List<ChatShortInfo> allChats = chatsManager.GetAllChats(searchUserId);
+
+            // Assert
+            Assert.AreEqual(2, allChats.Count);
+
+            ChatShortInfo firstChatInfo = allChats.FirstOrDefault(c => c.ChatID == 1);
+            Assert.IsNotNull(firstChatInfo);
+            Assert.AreEqual(1, firstChatInfo.PartnerID);
+            Assert.AreEqual("Hello!", firstChatInfo.LastMessage.Text);
+            Assert.AreEqual(2, firstChatInfo.LastMessage.SenderID);
+            Assert.IsTrue(firstChatInfo.LastMessage.IsRead);
+
+            ChatShortInfo secondChatInfo = allChats.FirstOrDefault(c => c.ChatID == 2);
+            Assert.IsNotNull(secondChatInfo);
+            Assert.AreEqual(3, secondChatInfo.PartnerID);
+            Assert.AreEqual("Fine)", secondChatInfo.LastMessage.Text);
+            Assert.AreEqual(2, secondChatInfo.LastMessage.SenderID);
+            Assert.IsFalse(secondChatInfo.LastMessage.IsRead);
+        }
+
+        [Test]
+        public void GetChatMessagesTest()
+        {
+            // Arrange
+            int userId = 2;
+            int chatId = 2;
+            _mockChatUnitOfWork.Setup(uow => uow.MessagesRepository.TakeOrdered(
+                It.IsAny<ExpressionSpecification<Message>>(),
+                It.IsAny<Func<IQueryable<Message>, IQueryable<Message>>>(),
+                It.IsAny<Func<Message,DateTime>>(),
+                It.IsAny<int?>(),
+                It.IsAny<int?>(),
+                It.IsAny<bool>()))
+                .Returns(_testChatEntities.First(c => c.ChatID == chatId).Messages.ToList());
+
+            var chatsManager = new ChatsManager(_mockChatUnitOfWork.Object);
+
+            // Act
+            List<MessageShortInfo> allChatMessages = chatsManager.GetChatMessages(userId, chatId);
+
+            // Assert
+            Assert.AreEqual(3, allChatMessages.Count);
+
+            MessageShortInfo firstMessage = allChatMessages.First();
+            Assert.AreEqual("Hi!", firstMessage.Text);
+            Assert.AreEqual(3, firstMessage.SenderID);
+            Assert.IsTrue(firstMessage.IsRead);
+
+            MessageShortInfo lastMessage = allChatMessages.First();
+            Assert.AreEqual("Hi!", lastMessage.Text);
+            Assert.AreEqual(3, lastMessage.SenderID);
+            Assert.IsTrue(lastMessage.IsRead);
         }
     }
 }
